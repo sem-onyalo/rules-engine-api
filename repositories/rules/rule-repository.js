7c69eebcbfb04cc6e9f52fd82d3118d7f4fc15c8
Sem Onyalo <sonyalo@loyalty.com>
2017-07-24 12:46:11 -0400

    COL-418: addedmethod to instantiate different rule classes by type

--- repositories/rules/rule-repository.js
+++ repositories/rules/rule-repository.js
@@ -1,5 +1,29 @@
 "use strict";
 
+const Models = require('../../models');
+
+const selectRuleSql = 'select r.ID, r.PARENT_RULE_ID, r.TYPE_RULE, r.SCORE, r.EMAIL_ON_FAIL '
+  + '    , rf.THRESHOLDCOUNT AS THRESHOLD_COUNT, rf.THRESHOLDMINUTE AS THRESHOLD_MINUTE '
+  + '    , raf.THRESHOLDCOUNT AS ACCOUNT_THRESHOLD_COUNT, raf.THRESHOLDMINUTE AS ACCOUNT_THRESHOLD_MINUTE, raf.ACCOUNTCOUNTTHRESHOLD AS ACCOUNT_COUNT_THRESHOLD '
+  + '    , rcc.LIST_COUNTRY_CODE '
+  + '    , rst.SCORE_THRESHOLD '
+  + '    , re.EMAILTO, re.EMAILSUBJECT, re.EMAILBODY '
+  + '    , CHILD_RULE.ID as CHILD_RULE_ID, CHILD_RULE.TYPE_RULE as CHILD_RULE_TYPE_RULE, CHILD_RULE.SCORE as CHILD_RULE_SCORE '
+  + '    , CHILD_RULE.EMAIL_ON_FAIL as CHILD_RULE_EMAIL_ON_FAIL, CHILD_RULE.EMAILTO AS CHILD_RULE_EMAILTO, CHILD_RULE.EMAILSUBJECT AS CHILD_RULE_EMAILSUBJECT '
+  + '    , CHILD_RULE.EMAILBODY AS CHILD_RULE_EMAILBODY '
+  + 'left join RULE_FREQUENCY rf on rf.RULE_ID = r.ID '
+  + 'left join RULE_ACCOUNT_FREQUENCY raf on raf.RULE_ID = r.ID '
+  + 'left join RULE_COUNTRY_CODE rcc on rcc.RULE_ID = r.ID '
+  + 'left join RULE_SCORE_THRESHOLD rst on rst.RULE_ID = r.ID '
+  + 'left join RULEEMAIL re on re.RULE_ID = r.ID '
+  + 'left join ( '
+  + '    select r.ID, r.PARENT_RULE_ID, r.TYPE_RULE, r.SCORE, r.EMAIL_ON_FAIL '
+  + '        , re.EMAILTO, re.EMAILSUBJECT, re.EMAILBODY '
+  + '    from RULE r '
+  + '    left join RULEEMAIL re on re.RULE_ID = r.ID '
+  + ') CHILD_RULE on CHILD_RULE.PARENT_RULE_ID = r.ID '
+  + 'from RULE r ';
+
 module.exports = class RuleRepository {
   constructor(dbContext) {
     this._dbContext = dbContext;
@@ -7,5 +31,96 @@ module.exports = class RuleRepository {
 
   selectById(id) {
     
+  }
+
+  getRuleFromDataSet(dataSet, colNamePrefix = '', rowIndex = 0) {
+    let ruleIdColName = colNamePrefix + 'ID';
+    let ruleTypeColName = colNamePrefix + 'TYPE_RULE';
+    let ruleScoreColName = colNamePrefix + 'SCORE';
+    let ruleEmailOnFailColName = colNamePrefix + 'EMAIL_ON_FAIL';
+    let ruleEmailToColName = colNamePrefix + 'EMAILTO';
+    let ruleEmailSubjectColName = colNamePrefix + 'EMAILSUBJECT';
+    let ruleEmailBodyColName = colNamePrefix + 'EMAILBODY';
+
+    let rule = null;
+    let ruleId = this._dbContext.getValueFromResultSet(dataSet, ruleIdColName, rowIndex);
+    let ruleType = this._dbContext.getValueFromResultSet(dataSet, ruleTypeColName, rowIndex);
+    let ruleScore = this._dbContext.getValueFromResultSet(dataSet, ruleScoreColName, rowIndex);
+    let ruleEmailOnFail = this._dbContext.getValueFromResultSet(dataSet, ruleEmailOnFailColName, rowIndex);
+    let ruleEmailTo = this._dbContext.getValueFromResultSet(dataSet, ruleEmailToColName, rowIndex);
+    let ruleEmailSubject = this._dbContext.getValueFromResultSet(dataSet, ruleEmailSubjectColName, rowIndex);
+    let ruleEmailBody = this._dbContext.getValueFromResultSet(dataSet, ruleEmailBodyColName, rowIndex);
+
+    switch (ruleType) {
+      case Models.Rules.RuleType.ACCOUNT_LOCKED:
+      case Models.Rules.RuleType.EMAIL_BLOCKLIST:
+      case Models.Rules.RuleType.DIFFERENT_EMAIL:
+        rule = new Models.Rules.Rule(ruleId, ruleScore, ruleType, ruleEmailOnFail, ruleEmailTo, ruleEmailSubject, ruleEmailBody);
+        return rule;
+
+      case Models.Rules.RuleType.SOURCE_IP:
+        rule = new Models.Rules.RuleSourceIp();
+        rule.Id = ruleId;
+        rule.Type = ruleType;
+        rule.Score = ruleScore;
+        rule.EmailOnFail = ruleEmailOnFail;
+        rule.EmailTo = ruleEmailTo;
+        rule.EmailSubject = ruleEmailSubject;
+        rule.EmailBody = ruleEmailBody;
+        rule.CountryCodes = [];
+        for (let i = 0; i < dataSet.rows.length; i++) {
+            rule.CountryCodes.push(this._dbContext.getValueFromResultSet(dataSet, 'LIST_COUNTRY_CODE', i));
+        }
+        return rule;
+
+      case Models.Rules.RuleType.ORDERS_CREATED:
+        rule = new Models.Rules.RuleFrequency();
+        rule.Id = ruleId;
+        rule.Type = ruleType;
+        rule.Score = ruleScore;
+        rule.EmailOnFail = ruleEmailOnFail;
+        rule.EmailTo = ruleEmailTo;
+        rule.EmailSubject = ruleEmailSubject;
+        rule.EmailBody = ruleEmailBody;
+        rule.ThresholdCount = this._dbContext.getValueFromResultSet(dataSet, 'THRESHOLD_COUNT');
+        rule.ThresholdMinutes = this._dbContext.getValueFromResultSet(dataSet, 'THRESHOLD_MINUTE');
+        return rule;
+
+      case Models.Rules.RuleType.REQUESTS_FROM_IP:
+        rule = new Models.Rules.RuleAccountFrequency();
+        rule.Id = ruleId;
+        rule.Type = ruleType;
+        rule.Score = ruleScore;
+        rule.EmailOnFail = ruleEmailOnFail;
+        rule.EmailTo = ruleEmailTo;
+        rule.EmailSubject = ruleEmailSubject;
+        rule.EmailBody = ruleEmailBody;
+        rule.ThresholdCount = this._dbContext.getValueFromResultSet(dataSet, 'ACCOUNT_THRESHOLD_COUNT');
+        rule.ThresholdMinutes = this._dbContext.getValueFromResultSet(dataSet, 'ACCOUNT_THRESHOLD_MINUTE');
+        rule.AccountCountThreshold = this._dbContext.getValueFromResultSet(dataSet, 'ACCOUNT_COUNT_THRESHOLD');
+        return rule;
+
+      case Models.Rules.RuleType.SCORE_THRESHOLD:
+        rule = new Models.Rules.RuleScoreThreshold();
+        rule.Id = ruleId;
+        rule.Type = ruleType;
+        rule.Score = ruleScore;
+        rule.EmailOnFail = ruleEmailOnFail;
+        rule.EmailTo = ruleEmailTo;
+        rule.EmailSubject = ruleEmailSubject;
+        rule.EmailBody = ruleEmailBody;
+        rule.Threshold = this._dbContext.getValueFromResultSet(dataSet, 'SCORE_THRESHOLD');
+        rule.ChildRules = [];
+        for (let i = 0; i < dataSet.rows.length; i++) {
+          let childRule = this.getRuleFromDataSet(dataSet, 'CHILD_RULE_', i);
+          if (childRule !== null) {
+            rule.ChildRules.push(childRule);
+          }
+        }
+        return rule;
+
+      default:
+        return null;
+    }
   }
 }
--- repositories/rules/rule-repository.spec.js
+++ repositories/rules/rule-repository.spec.js
@@ -3,13 +3,162 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
 
+const DbContext = require('../db-context');
+const Models = require('../../models');
 const RuleRepository = require('./rule-repository');
 
 describe('RuleRepository', () => {
+  let emailTo = 'fraudteam@nomail.com';
+  let emailSubject = 'Fraud Service Email';
+  let emailBody = 'A fraud rule set ran';
+
+  let dbContext;
+  let ruleRepository;
+
+  beforeEach(() => {
+    dbContext = new DbContext();
+    ruleRepository = new RuleRepository(dbContext);
+  });
+
   describe('selectById(id)', () => {
     it('should export function', () => {
-      let ruleRepository = new RuleRepository();
       expect(ruleRepository.selectById).to.be.a('function');
+    });
+  });
+
+  describe('getRuleFromDataSet(args)', () => {
+    it('should export function', () => {
+      expect(ruleRepository.getRuleFromDataSet).to.be.a('function');
+    });
+
+    it('should return null if unsupported type', () => {
+      let ruleType = 0;
+      let dataSet = {
+        rows: [ [ 123, 0, ruleType, 10, false ] ],
+        metaData: [ { name: 'ID' }, { name: 'PARENT_RULE_ID' }, { name: 'TYPE_RULE' }, { name: 'SCORE' }, { name: 'EMAIL_ON_FAIL' } ]
+      };
+
+      let rule = ruleRepository.getRuleFromDataSet(dataSet);
+
+      assert.isNull(rule, 'Returned rule should be null');
+    });
+
+    it('should return an instance of Rule', () => {
+      let types = [
+        Models.Rules.RuleType.ACCOUNT_LOCKED,
+        Models.Rules.RuleType.EMAIL_BLOCKLIST,
+        Models.Rules.RuleType.DIFFERENT_EMAIL
+      ];
+
+      for (let i = 0; i < types.length; i++) {
+        let dataSet = {
+          rows: [ [ 123, 0, types[i], 10, false, emailTo, emailSubject, emailBody ] ],
+          metaData: [
+            { name: 'ID' }, { name: 'PARENT_RULE_ID' }, { name: 'TYPE_RULE' }, { name: 'SCORE' }, { name: 'EMAIL_ON_FAIL' },
+            { name: 'EMAILTO' }, { name: 'EMAILSUBJECT' }, { name: 'EMAILBODY' }
+          ]
+        };
+
+        let expectedRule = new Models.Rules.Rule(123, 10, types[i], false, emailTo, emailSubject, emailBody);
+        let rule = ruleRepository.getRuleFromDataSet(dataSet);
+
+        assert.deepEqual(rule, expectedRule, 'Returned rule was not expected value for rule type ' + types[i]);
+      }
+    });
+
+    it('should return an instance of RuleSourceIp', () => {
+      let dataSet = {
+        rows: [
+          [ 123, 0, Models.Rules.RuleType.SOURCE_IP, 10, false, 'CA', emailTo, emailSubject, emailBody ],
+          [ 123, 0, Models.Rules.RuleType.SOURCE_IP, 10, false, 'US', emailTo, emailSubject, emailBody ]
+        ],
+        metaData: [
+          { name: 'ID' }, { name: 'PARENT_RULE_ID' }, { name: 'TYPE_RULE' }, { name: 'SCORE' }, { name: 'EMAIL_ON_FAIL' }, { name: 'LIST_COUNTRY_CODE' } ,
+          { name: 'EMAILTO' }, { name: 'EMAILSUBJECT' }, { name: 'EMAILBODY' }
+        ]
+      };
+
+      let expectedRule = new Models.Rules.RuleSourceIp(123, 10, ['CA', 'US']);
+      expectedRule.Type = Models.Rules.RuleType.SOURCE_IP;
+      expectedRule.EmailOnFail = false;
+      expectedRule.EmailTo = emailTo;
+      expectedRule.EmailSubject = emailSubject;
+      expectedRule.EmailBody = emailBody;
+      let rule = ruleRepository.getRuleFromDataSet(dataSet);
+
+      assert.deepEqual(rule, expectedRule, 'Returned rule was not expected value');
+    });
+
+    it('should return an instance of RuleFrequency', () => {
+      let dataSet = {
+        rows: [ [ 123, 0, Models.Rules.RuleType.ORDERS_CREATED, 10, false, 2, 180, emailTo, emailSubject, emailBody ] ],
+        metaData: [
+          { name: 'ID' }, { name: 'PARENT_RULE_ID' }, { name: 'TYPE_RULE' }, { name: 'SCORE' }, { name: 'EMAIL_ON_FAIL' },
+          { name: 'THRESHOLD_COUNT' }, { name: 'THRESHOLD_MINUTE' }, { name: 'EMAILTO' }, { name: 'EMAILSUBJECT' }, { name: 'EMAILBODY' }
+        ]
+      };
+
+      let ruleThresholdCount = 2, ruleThresholdMin = 180;
+      let expectedRule = new Models.Rules.RuleFrequency(123, 10, ruleThresholdCount, ruleThresholdMin);
+      expectedRule.Type = Models.Rules.RuleType.ORDERS_CREATED;
+      expectedRule.EmailOnFail = false;
+      expectedRule.EmailTo = emailTo;
+      expectedRule.EmailSubject = emailSubject;
+      expectedRule.EmailBody = emailBody;
+      let rule = ruleRepository.getRuleFromDataSet(dataSet);
+
+      assert.deepEqual(rule, expectedRule, 'Returned rule was not expected value');
+    });
+
+    it('should return an instance of RuleAccountFrequency', () => {
+      let dataSet = {
+        rows: [ [ 123, 0, Models.Rules.RuleType.REQUESTS_FROM_IP, 10, false, 1, 180, 2, emailTo, emailSubject, emailBody ] ],
+        metaData: [
+          { name: 'ID' }, { name: 'PARENT_RULE_ID' }, { name: 'TYPE_RULE' }, { name: 'SCORE' }, { name: 'EMAIL_ON_FAIL' },
+          { name: 'ACCOUNT_THRESHOLD_COUNT' }, { name: 'ACCOUNT_THRESHOLD_MINUTE' }, { name: 'ACCOUNT_COUNT_THRESHOLD' },
+          { name: 'EMAILTO' }, { name: 'EMAILSUBJECT' }, { name: 'EMAILBODY' }
+        ]
+      };
+
+      let ruleThresholdCount = 1, ruleThresholdMin = 180, accountCountThreshold = 2;
+      let expectedRule = new Models.Rules.RuleAccountFrequency(123, 10, ruleThresholdCount, ruleThresholdMin, accountCountThreshold);
+      expectedRule.Type = Models.Rules.RuleType.REQUESTS_FROM_IP;
+      expectedRule.EmailOnFail = false;
+      expectedRule.EmailTo = emailTo;
+      expectedRule.EmailSubject = emailSubject;
+      expectedRule.EmailBody = emailBody;
+      let rule = ruleRepository.getRuleFromDataSet(dataSet);
+
+      assert.deepEqual(rule, expectedRule, 'Returned rule was not expected value');
+    });
+
+    it('should return an instance of RuleScoreThreshold', () => {
+      let dataSet = {
+        rows: [
+          [ 123, 0, Models.Rules.RuleType.SCORE_THRESHOLD, 10, false, 15, emailTo, emailSubject, emailBody, null, null, null, null ],
+          [ 123, 0, Models.Rules.RuleType.SCORE_THRESHOLD, 10, false, 15, emailTo, emailSubject, emailBody, 124, Models.Rules.RuleType.ACCOUNT_LOCKED, 0, false ],
+          [ 123, 0, Models.Rules.RuleType.SCORE_THRESHOLD, 10, false, 15, emailTo, emailSubject, emailBody, 125, Models.Rules.RuleType.EMAIL_BLOCKLIST, 1, false ]
+        ],
+        metaData: [
+          { name: 'ID' }, { name: 'PARENT_RULE_ID' }, { name: 'TYPE_RULE' }, { name: 'SCORE' }, { name: 'EMAIL_ON_FAIL' },
+          { name: 'SCORE_THRESHOLD' }, { name: 'EMAILTO' }, { name: 'EMAILSUBJECT' }, { name: 'EMAILBODY' },
+          { name: 'CHILD_RULE_ID' }, { name: 'CHILD_RULE_TYPE_RULE' }, { name: 'CHILD_RULE_SCORE' }, { name: 'CHILD_RULE_EMAIL_ON_FAIL' }
+        ]
+      };
+
+      let scoreThreshold = 15;
+      let childRules = [
+        new Models.Rules.Rule(124, 0, Models.Rules.RuleType.ACCOUNT_LOCKED, false),
+        new Models.Rules.Rule(125, 1, Models.Rules.RuleType.EMAIL_BLOCKLIST, false)
+      ];
+      let expectedRule = new Models.Rules.RuleScoreThreshold(123, Models.Rules.RuleType.SCORE_THRESHOLD, 10, scoreThreshold, childRules);
+      expectedRule.EmailOnFail = false;
+      expectedRule.EmailTo = emailTo;
+      expectedRule.EmailSubject = emailSubject;
+      expectedRule.EmailBody = emailBody;
+      let rule = ruleRepository.getRuleFromDataSet(dataSet);
+
+      assert.deepEqual(rule, expectedRule, 'Returned rule was not expected value');
     });
   });
 });
